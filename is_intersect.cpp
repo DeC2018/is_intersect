f70ffc1754e2d2e33e895dbc2b5491a457750f90
Den <emailfordenis@yandex.ru>
2023-07-07 05:59:44 +1000

    Add project files.

---
+++ is_intersect.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+struct Rectangle;
+struct Triangle;
+struct Circle;
+
+bool is_intersect(Rectangle * a, Rectangle * b) { std::cout << "R x R\n"; return true; }
+bool is_intersect(Rectangle * a, Triangle  * b) { std::cout << "R x T\n"; return true; }
+bool is_intersect(Rectangle * a, Circle    * b) { std::cout << "R x C\n"; return true; }
+bool is_intersect(Triangle  * a, Triangle  * b) { std::cout << "T x T\n"; return true; }
+bool is_intersect(Triangle  * a, Circle    * b) { std::cout << "T x C\n"; return true; }
+bool is_intersect(Circle    * a, Circle    * b) { std::cout << "C x C\n"; return true; }
+
+struct Shape
+{
+    virtual ~Shape() {}
+    virtual bool intersect( Rectangle * r ) = 0;
+    virtual bool intersect( Triangle  * t ) = 0;
+    virtual bool intersect( Circle    * c ) = 0;
+    virtual bool intersect( Shape     * s ) = 0;
+};
+
+struct Rectangle : Shape
+{
+    bool intersect( Rectangle * r ) { return is_intersect(this, r); }
+    bool intersect( Triangle  * t ) { return is_intersect(this, t); }
+    bool intersect( Circle    * c ) { return is_intersect(this, c); }
+    bool intersect( Shape     * s ) {
+        return s->intersect(this);
+    }
+};
+
+struct Triangle : Shape
+{
+    bool intersect( Rectangle * r ) { return is_intersect(r, this); }
+    bool intersect( Triangle  * t ) { return is_intersect(t, this); }
+    bool intersect( Circle    * c ) { return is_intersect(this, c); }
+    bool intersect( Shape     * s ) {
+        return s->intersect(this);
+    }
+};
+
+struct Circle : Shape
+{
+    bool intersect( Rectangle * r ) { return is_intersect(r, this); }
+    bool intersect( Triangle  * t ) { return is_intersect(t, this); }
+    bool intersect( Circle    * c ) { return is_intersect(c, this); }
+    bool intersect( Shape     * s ) {
+        return s->intersect(this);
+    }
+};
+
+bool is_intersect(Shape * a, Shape * b)
+{
+    return a->intersect(b);
+}
+
+int main(){
+    Rectangle r; Circle c;
+    is_intersect(&r, &c);
+}
